6c858a34f52c599cd4c7552c0b71639f3ba8b7ae
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2018-01-16 02:58:27 +0100

    FRP!

--- src/lib.rs
+++ src/lib.rs
@@ -1,171 +1,219 @@
 use std::cell::RefCell;
-use std::ops::Deref;
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 
-pub trait Bidule {
-  // Type of signal this `Bidule` can emit.
-  type Signal;
+type Subscribers<Sig> = RefCell<Vec<Box<FnMut(&Sig)>>>;
 
-  // React to an external event.
-  //fn react<E, S>(&mut self, event: &E, app_st: &mut S);
-
-  // Add a subscriber to listen to signals coming out of this `Bidule`.
-  fn subscribe<F>(&mut self, subscriber: F) where F: 'static + FnMut(&Self::Signal);
+enum SubscribersRef<Sig> {
+  Own(Rc<Subscribers<Sig>>),
+  Weak(Weak<Subscribers<Sig>>)
 }
 
-pub struct Shared<T>(Rc<RefCell<T>>);
-
-impl<T> Shared<T> {
-  pub fn new(x: T) -> Self {
-    Shared(Rc::new(RefCell::new(x)))
-  }
+/// A stream of signals.
+///
+/// A stream represents a composable signal producer. When you decide to send a signal down a
+/// stream, any other streams composed with that first stream will also receive the signal. This
+/// enables to construct more interesting and complex streams by composing them.
+pub struct Stream<Sig> {
+  subscribers: SubscribersRef<Sig>
 }
 
-impl<T> Clone for Shared<T> {
-  fn clone(&self) -> Self {
-    Shared(self.0.clone())
-  }
-}
-
-impl<T> Deref for Shared<T> {
-  type Target = Rc<RefCell<T>>;
-
-  fn deref(&self) -> &Self::Target {
-    &self.0
-  }
-}
-
-// Text label.
-pub struct Label {
-  text: String,
-  subscribers: Vec<Box<FnMut(&LabelSignal)>>
-}
-
-impl Label {
-  pub fn new() -> Shared<Self> {
-    Self::with_text("")
+impl<Sig> Stream<Sig> where Sig: 'static {
+  /// Create a new stream.
+  pub fn new() -> Self {
+    let subscribers = SubscribersRef::Own(Rc::new(RefCell::new(Vec::new())));
+    Stream { subscribers }
   }
 
-  pub fn with_text(text: &str) -> Shared<Self> {
-    let label =
-      Label {
-        text: text.to_owned(),
-        subscribers: Vec::new()
-      };
+  /// Create a new, non-owned, version of this stream.
+  fn new_weak(&self) -> Self {
+    let subscribers = match self.subscribers {
+      SubscribersRef::Own(ref rc) => SubscribersRef::Own(rc.clone()),
+      SubscribersRef::Weak(ref weak) => SubscribersRef::Weak(weak.clone())
+    };
 
-    Shared::new(label)
+    Stream { subscribers }
   }
 
-  fn notify(&mut self, signal: LabelSignal) {
-    for sub in &mut self.subscribers {
-      sub(&signal);
+  /// Subscribe a new listener for this stream’s signals.
+  ///
+  /// This function enables to “observe” any signal flowing out of the stream. However, do not abuse
+  /// this function, as its primary use is to build other combinators.
+  pub fn subscribe<F>(&self, subscriber: F) where F: 'static + FnMut(&Sig) {
+    match self.subscribers {
+      SubscribersRef::Own(ref subscribers) => subscribers.borrow_mut().push(Box::new(subscriber)),
+      SubscribersRef::Weak(ref weak) => {
+        if let Some(subscribers) = weak.upgrade() {
+          subscribers.borrow_mut().push(Box::new(subscriber));
+        }
+      }
     }
   }
 
-  pub fn as_str(&self) -> &str {
-    &self.text
-  }
-}
+  /// Send a signal down the stream.
+  pub fn send(&self, signal: &Sig) {
+    match self.subscribers {
+      SubscribersRef::Own(ref subscribers) => {
+        for sub in subscribers.borrow_mut().iter_mut() {
+          sub(signal);
+        }
+      }
 
-impl Shared<Label> {
-  pub fn set(&mut self, text: &str) {
-    let mut self_ = self.borrow_mut();
-
-    self_.text = text.to_owned();
-    self_.notify(LabelSignal::TextChanged(text.to_owned()));
-  }
-
-  pub fn reset(&mut self) {
-    self.set("")
-  }
-
-  pub fn submit(&mut self) {
-    self.borrow_mut().notify(LabelSignal::Submit);
-  }
-}
-
-impl Bidule for Shared<Label> {
-  type Signal = LabelSignal;
-
-  fn subscribe<F>(&mut self, subscriber: F) where F: 'static + FnMut(&Self::Signal) {
-    self.0.borrow_mut().subscribers.push(Box::new(subscriber));
-  }
-}
-
-// Signals that can be emited by a `Label`.
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub enum LabelSignal {
-  TextChanged(String),
-  Submit
-}
-
-// On/off button.
-pub struct ToggleButton {
-  pushed: bool,
-  subscribers: Vec<Box<FnMut(&ToggleButtonSignal)>>
-}
-
-impl ToggleButton {
-  pub fn new() -> Shared<Self> {
-    Self::with_pushed(false)
-  }
-
-  pub fn with_pushed(pushed: bool) -> Shared<Self> {
-    let button =
-      ToggleButton {
-        pushed,
-        subscribers: Vec::new()
-      };
-
-    Shared::new(button)
-  }
-
-  fn notify(&mut self, signal: ToggleButtonSignal) {
-    for sub in &mut self.subscribers {
-      sub(&signal);
+      SubscribersRef::Weak(ref weak) => {
+        if let Some(subscribers) = weak.upgrade() {
+          for sub in subscribers.borrow_mut().iter_mut() {
+            sub(signal);
+          }
+        }
+      }
     }
   }
+
+  /// Map any signals flowing out a stream.
+  ///
+  /// Please note that this function is total: you cannot ignore signals. Even if you map
+  /// *uninteresting signals* to `None`, you’ll still compose signals for those. If are interested
+  /// in filtering signals while mapping, have a look at the `filter_map` function.
+  pub fn map<F, OutSig>(
+    &self,
+    f: F
+  ) -> Stream<OutSig>
+    where F: 'static + Fn(&Sig) -> OutSig,
+          OutSig: 'static {
+    let mapped_stream = Stream::new();
+    let mapped_stream_ = mapped_stream.new_weak();
+
+    self.subscribe(move |sig| {
+      mapped_stream_.send(&f(sig));
+    });
+
+    mapped_stream
+  }
+
+  /// Filter and map signals flowing out a stream.
+  ///
+  /// If you’re not interested in a specific signal, you can emit `None`: no signal will be sent.
+  pub fn filter_map<F, OutSig>(
+    &self,
+    f: F
+  ) -> Stream<OutSig>
+    where F: 'static + Fn(&Sig) -> Option<OutSig>,
+          OutSig: 'static {
+    let mapped_stream = Stream::new();
+    let mapped_stream_ = mapped_stream.new_weak();
+
+    self.subscribe(move |sig| {
+      if let Some(ref mapped_sig) = f(sig) {
+        mapped_stream_.send(mapped_sig);
+      }
+    });
+
+    mapped_stream
+  }
+
+  /// Filter the signals flowing out of a stream with a predicate.
+  pub fn filter<F>(&self, pred: F) -> Self where F: 'static + Fn(&Sig) -> bool {
+    let filtered = Stream::new();
+    let filtered_ = filtered.new_weak();
+
+    self.subscribe(move |sig| {
+      if pred(sig) {
+        filtered_.send(sig);
+      }
+    });
+
+    filtered
+  }
+
+  /// Fold all signals flowing out of a stream into a stream of values.
+  pub fn fold<F, A>(
+    &self,
+    value: A,
+    f: F
+  ) -> Stream<A>
+    where F: 'static + Fn(A, &Sig) -> A,
+          A: 'static {
+    let folded_stream = Stream::new();
+    let folded_stream_ = folded_stream.new_weak();
+    let mut boxed = Some(value);
+
+    self.subscribe(move |sig| {
+      if let Some(value) = boxed.take() {
+        let output = f(value, sig);
+        folded_stream_.send(&output);
+        boxed = Some(output);
+      }
+    });
+
+    folded_stream
+  }
+
+  /// Merge two streams into one.
+  ///
+  /// Merging streams enables you to perform later useful compositions, such as folding the merged
+  /// results.
+  pub fn merge(&self, rhs: &Self) -> Self {
+    let merged = Stream::new();
+    let merged_self = merged.new_weak();
+    let merged_rhs = merged.new_weak();
+
+    self.subscribe(move |sig| {
+      merged_self.send(sig);
+    });
+
+    rhs.subscribe(move |sig| {
+      merged_rhs.send(sig);
+    });
+
+    merged
+  }
+
+  /// Create a pair of entangled streams.
+  ///
+  /// If any of the streams sends a signal, the other one receives it. However, be careful: since
+  /// the signals are defined in terms of each other, it’s quite easy to cause infinite loops if you
+  /// don’t have a well-defined bottom to your recursion. This is why you’re expected to return
+  /// `Option<_>` signals.
+  pub fn entangled<F, G, GSig>(
+    f: F,
+    g: G
+  ) -> (Self, Stream<GSig>)
+    where F: 'static + Fn(&Sig) -> Option<GSig>,
+          G: 'static + Fn(&GSig) -> Option<Sig>,
+          GSig: 'static {
+    let fs = Stream::new();
+    let gs = Stream::new();
+    let fs_ = fs.new_weak();
+    let gs_ = gs.new_weak();
+
+    fs.subscribe(move |sig| {
+      if let Some(sig_) = f(sig) {
+        gs_.send(&sig_);
+      }
+    });
+
+    gs.subscribe(move |sig| {
+      if let Some(sig_) = g(sig) {
+        fs_.send(&sig_);
+      }
+    });
+
+    (fs, gs)
+  }
 }
 
-impl Shared<ToggleButton> {
-  pub fn push(&mut self) {
-    let mut self_ = self.0.borrow_mut();
+impl<SigA, SigB> Stream<(SigA, SigB)> where SigA: 'static, SigB: 'static {
+  /// Split a stream of merged values into two streams.
+  pub fn unmerge(&self) -> (Stream<SigA>, Stream<SigB>) {
+    let a = Stream::new();
+    let a_ = a.new_weak();
+    let b = Stream::new();
+    let b_ = b.new_weak();
 
-    self_.pushed = true;
-    self_.notify(ToggleButtonSignal::Pressed);
+    self.subscribe(move |sig| {
+      a_.send(&sig.0);
+      b_.send(&sig.1);
+    });
+
+    (a, b)
   }
-
-  pub fn release(&mut self) {
-    let mut self_ = self.0.borrow_mut();
-
-    self_.pushed = false;
-    self_.notify(ToggleButtonSignal::Released);
-  }
-
-  pub fn toggle(&mut self) {
-    let mut self_ = self.0.borrow_mut();
-
-    if self_.pushed {
-      self_.pushed = false;
-      self_.notify(ToggleButtonSignal::Released);
-    } else {
-      self_.pushed = true;
-      self_.notify(ToggleButtonSignal::Pressed);
-    }
-  }
-}
-
-impl Bidule for Shared<ToggleButton> {
-  type Signal = ToggleButtonSignal;
-
-  fn subscribe<F>(&mut self, subscriber: F) where F: 'static + FnMut(&Self::Signal) {
-    self.0.borrow_mut().subscribers.push(Box::new(subscriber));
-  }
-}
-
-// Signals that can be emited by a `ToggleButton`.
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub enum ToggleButtonSignal {
-  Pressed,
-  Released
 }
--- tests/mod.rs
+++ tests/mod.rs
@@ -3,43 +3,118 @@ extern crate bidule;
 use bidule::*;
 
 #[test]
-fn test_0() {
-  let label_0 = Label::new();
-  let mut label_0_ = label_0.clone();
-  let mut label = Label::new();
+fn frp() {
+  use std::cell::RefCell;
+  use std::rc::Rc;
 
-  label.subscribe(move |signal| {
-    match *signal {
-      LabelSignal::TextChanged(ref txt) => label_0_.set(txt),
-      LabelSignal::Submit => label_0_.reset()
+  enum Button {
+    Push
+  }
+
+  enum Counter {
+    Increment,
+    Decrement,
+    Reset
+  }
+
+  let minus_button: Stream<Button> = Stream::new();
+  let plus_button: Stream<Button> = Stream::new();
+  let reset_button: Stream<Button> = Stream::new();
+
+  let minus = minus_button.map(|_| Counter::Decrement);
+  let plus = plus_button.map(|_| Counter::Increment);
+  let reset = reset_button.map(|_| Counter::Reset);
+
+  let label = minus.merge(&plus).merge(&reset).fold(0, |x, sig| {
+    match *sig {
+      Counter::Increment => x + 1,
+      Counter::Decrement => x - 1,
+      Counter::Reset => 0
     }
-  });
+  }).filter(|x| x % 2 == 0); // get only even counters
 
-  label.set("coucou");
-  assert_eq!(label_0.borrow().as_str(), "coucou");
+  // FIXME: this part is ugly, we need to find a better API for “inspecting” streams
+  let counter = Rc::new(RefCell::new(0));
+  let counter_ = counter.clone();
+  label.subscribe(move |x| *counter_.borrow_mut() = *x);
 
-  label.submit();
-  assert_eq!(label_0.borrow().as_str(), "");
+  assert_eq!(*counter.borrow(), 0);
+
+  minus_button.send(&Button::Push);
+  assert_eq!(*counter.borrow(), 0); // not -1 because -1 is odd
+
+  minus_button.send(&Button::Push);
+  assert_eq!(*counter.borrow(), -2);
+
+  plus_button.send(&Button::Push);
+  assert_eq!(*counter.borrow(), -2); // not -1, it’s odd
+
+  reset_button.send(&Button::Push);
+  assert_eq!(*counter.borrow(), 0);
 }
 
 #[test]
-fn test_1() {
-  let label = Label::new();
-  let mut label_0 = label.clone();
-  let mut button = ToggleButton::new();
+fn mutual_recursion() {
+  use std::cell::RefCell;
+  use std::rc::Rc;
 
-  button.subscribe(move |signal| {
-    match *signal {
-      ToggleButtonSignal::Pressed => label_0.set("hey!"),
-      ToggleButtonSignal::Released => label_0.reset()
+  #[derive(Clone, Debug, Eq, PartialEq)]
+  enum Button {
+    ChangeLabel(String),
+    Push
+  }
+
+  fn f(sig: &Button) -> Option<Button> {
+    match *sig {
+      Button::Push => Some(Button::ChangeLabel("foo".to_owned())),
+      _ => None
     }
-  });
+  }
 
-  assert_eq!(label.borrow().as_str(), "");
+  let (x, y) = Stream::entangled(f, f);
 
-  button.push();
-  assert_eq!(label.borrow().as_str(), "hey!");
-  
-  button.release();
-  assert_eq!(label.borrow().as_str(), "");
+  let x_ref = Rc::new(RefCell::new(Button::Push));
+  let x_ref_ = x_ref.clone();
+
+  let y_ref = Rc::new(RefCell::new(Button::Push));
+  let y_ref_ = y_ref.clone();
+
+  x.subscribe(move |a| *x_ref_.borrow_mut() = a.clone());
+  y.subscribe(move |a| *y_ref_.borrow_mut() = a.clone());
+
+  assert_eq!(*x_ref.borrow(), Button::Push);
+  assert_eq!(*y_ref.borrow(), Button::Push);
+
+  x.send(&Button::Push);
+  assert_eq!(*x_ref.borrow(), Button::Push);
+  assert_eq!(*y_ref.borrow(), Button::ChangeLabel("foo".to_owned()));
+
+  y.send(&Button::Push);
+  assert_eq!(*x_ref.borrow(), Button::ChangeLabel("foo".to_owned()));
+  assert_eq!(*y_ref.borrow(), Button::Push);
+}
+
+#[test]
+fn unmerge() {
+  use std::cell::RefCell;
+  use std::rc::Rc;
+
+  let tuple = Stream::new();
+  let (x, y) = tuple.unmerge();
+
+  let x_ref = Rc::new(RefCell::new(0));
+  let x_ref_ = x_ref.clone();
+
+  let y_ref = Rc::new(RefCell::new(0));
+  let y_ref_ = y_ref.clone();
+
+  x.subscribe(move |a| *x_ref_.borrow_mut() = *a);
+  y.subscribe(move |a| *y_ref_.borrow_mut() = *a);
+
+  assert_eq!(*x_ref.borrow(), 0);
+  assert_eq!(*y_ref.borrow(), 0);
+
+  tuple.send(&(34, 13));
+  assert_eq!(*x_ref.borrow(), 34);
+  assert_eq!(*y_ref.borrow(), 13);
 }
